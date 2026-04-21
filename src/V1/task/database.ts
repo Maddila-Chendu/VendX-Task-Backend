5873fafabea895c50cef29bbf370bfb281c4c831
Maddila-Chendu <maddilachendu1234@gmail.com>
2026-04-21 14:51:35 +0530

    Add files

--- src/V1/task/database.ts
+++ src/V1/task/database.ts
@@ -1,9 +1,11 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "Task-Manager-DB",
-  password: "Chendu",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
